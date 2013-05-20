8831812bca3a3d783c6773aef89ad1f9a5fd3c7f
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2013-05-20 12:11:12 +0000

    Initial versions of XSLT files to convert LabVIEW ExportVIStrings() output to LVDCOM XML input and EPICS DB files
    Former-commit-id: ebb727f0b4adcca7f9245a2fc79ec58484bdbcbe

---
+++ lvDCOMApp/src/fix_xml.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM $Id$
+REM @author Freddie Akeroyd, STFC ISIS Facility, UK
+REM fix the XML output by the LabVIEW ExportVIStrings() method (which is more like HTML 2.0 than XML!)
+REM
+REM Usage:   fix_xml original_lvexport.txt lvexport.xml
+REM
+sed -e "s/\([A-Za-z0-9][A-Za-z0-9]*=\)\([A-Za-z0-9][A-Za-z0-9]*\)/\1\"\2\"/g" -e "s/<FONT[^>]*>//g" -e "s/<LF>//g" -e "s/<SAME_AS_LABEL>//g" -e "s/<NO_TITLE[^>]*>//g" -e "s/<<B>>//g" -e "s/<<\/B>>//g" %1 > %2
