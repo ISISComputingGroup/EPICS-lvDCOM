256aff8609d23d3e3fe6db09d697f069894bacca
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2013-06-04 10:24:12 +0000

    Remove <<APPEND>> etc from xml
    Former-commit-id: 9068fc070d00e18f1a1011af60f77f490268cf10

--- lvDCOMApp/src/fix_xml.cmd
+++ lvDCOMApp/src/fix_xml.cmd
@@ -5,4 +5,4 @@ REM fix the XML output by the LabVIEW ExportVIStrings() method (which is more li
 REM
 REM Usage:   fix_xml original_lvexport.txt lvexport.xml
 REM
-sed -e "s/\([A-Za-z0-9][A-Za-z0-9]*=\)\([A-Za-z0-9][A-Za-z0-9]*\)/\1\"\2\"/g" -e "s/<FONT[^>]*>//g" -e "s/<LF>//g" -e "s/<SAME_AS_LABEL>//g" -e "s/<NO_TITLE[^>]*>//g" -e "s/<<B>>//g" -e "s/<<\/B>>//g" %1 > %2
+sed -e "s/\([A-Za-z0-9][A-Za-z0-9]*=\)\([A-Za-z0-9][A-Za-z0-9]*\)/\1\"\2\"/g" -e "s/<FONT[^>]*>//g" -e "s/<LF>//g" -e "s/<SAME_AS_LABEL>//g" -e "s/<NO_TITLE[^>]*>//g" -e "s/<<[^>]*>>//g" %1 > %2
